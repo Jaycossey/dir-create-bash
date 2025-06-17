a85132656747ebe860e5da08f97b30c2f9196f27
Ian Scott-McInally <118208278+Jaycossey@users.noreply.github.com>
2025-06-17 10:34:59 +0100

    Create createPTDir.sh

---
+++ createPTDir.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo -n "Enter Company Name: "
+read dirName
+
+
+printf "Generating Directories...\n"
+
+mkdir $dirName && mkdir -vp $dirName/{EPT,IPT/{evidence,logs,scans,scope,tools}} && mkdir -vp $dirName/{EPT,IPT/evidence/{credentials,data,screenshots}} && mkdir -vp $dirName/Documents/{writeUps,reports,appendicies,summary}
+
+if [ -d "$dirName/EPT/evidence" ]; then echo "Directory created successfully" else echo "Error"; fi
+
+printf "Generating README...\n"
+
+echo "# $dirName - Pentest Report Introduction\n## Author - An0nyM0u53\n" > $dirName/Documents/README.md
+
+cat $dirName/Documents/README.md
+
+tree $dirName
