c6d9376b85aa59ef543f0e6e0bf57e8dcd9681f3
An0nyM0u53 <118208278+Jaycossey@users.noreply.github.com>
2025-07-26 19:24:58 +0100

    Update createPTDir.sh
    Better structure

--- createPTDir.sh
+++ createPTDir.sh
@@ -1,19 +1,38 @@
 #! /bin/bash
-
+# Read company name and store params
 echo -n "Enter Company Name: "
 read dirName
 
 
+#UI 
 printf "Generating Directories...\n"
 
-mkdir $dirName && mkdir -vp $dirName/{EPT,IPT}/{evidence,logs,scans,scope,tools} && mkdir -vp $dirName/{EPT,IPT}/evidence/{credentials,data,screenshots} && mkdir -vp $dirName/Documents/{writeUps,reports,appendicies,summary}
 
-if [ -d "$dirName/EPT/evidence" ]; then echo "Directory created successfully" else echo "Error"; fi
+# populate directories
+mkdir $dirName && mkdir -vp $dirName/{Documents,Writeups,References,OSINT,ExternalPT,InternalPT}
+mkdir $dirName/References/{Legal,CodeOfEthics}
+mkdir $dirName/OSINT/{Company,Infrastructure,Leaks}
+mkdir -vp $dirName/{ExternalPT,InternalPT}/evidence/{credentials,data,screenshots}
+mkdir -vp $dirName/Documents/{writeUps,reports,appendicies,summary}
 
+
+# Error handling
+if [ -d "$dirName/EPT/evidence" ]; then echo "Directory created successfully" else echo "Error" && exit 1; fi
+
+
+# UI
 printf "Generating README...\n"
 
-echo "# $dirName - Pentest Report Introduction\n## Author - An0nyM0u53\n" > $dirName/Documents/README.md
 
+# Create README
+echo "# $dirName - Pentest Report Introduction\n## Author - An0nyM0u53\n" > $dirName/Documents/README.md
+printf "Enter filepath for Code of Ethics...\n"
+read fileCoe
+
+cp $fileCoe $dirName/References/CodeOfEthics/CoE.md
+
+# UI 
 cat $dirName/Documents/README.md
 
+# UI
 tree $dirName
